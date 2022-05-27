de25405d06ce71858ea17439f78db79fe1738af1
Orhie <orhieasoro@gmail.com>
2022-05-27 15:12:29 +0100

    my blog

--- blogapp/views.py
+++ blogapp/views.py
@@ -12,17 +12,20 @@ def post_list(request, tag_slug=None):
     tag = None
     if tag_slug:
         tag = get_object_or_404(Tag, slug=tag_slug)
+        
         object_list = object_list.filter(tags__in=[tag])
+                
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-            # If page is not an integer deliver the first page
-            posts = paginator.page(1)
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
     except EmptyPage:
-            # If page is out of range deliver last page of results
-            posts = paginator.page(paginator.num_pages)
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
        
@@ -49,13 +52,13 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
     # List of similar posts
     post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
-            .exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
- .order_by('-same_tags','-publish')[:4]
-    return render(request,'blog/post/detail.html',{'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form, 'similiar_posts': similar_posts})
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    # print(similar_posts)
+    return render(request,'blog/post/detail.html',{'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form, 'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
